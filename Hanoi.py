8b65133543cd8a23c0c667f425041c4ea2de0fc7
brandonvessel <43678349+brandonvessel@users.noreply.github.com>
2019-10-09 13:17:52 -0500

    Add files via upload

--- Hanoi.py
+++ Hanoi.py
@@ -1,20 +1,27 @@
 from time import sleep
+import os, sys
 
 ######################
 ####### CONFIG #######
 ######################
 
 CLEAR_SCREEN = True     # default true
-disk_num = 7            # default 7
 disk_speed = 0.12       # default 0.12
 
 ######################
 ### Initialization ###
 ######################
 
-# Optional import (don't import if not needed)
-if(CLEAR_SCREEN):
-    import os
+# Check if disk_num specified
+if(len(sys.argv) < 2):
+    print("Usage: py {} <disk_num>".format(sys.argv[0]))
+    exit()
+else:
+    disk_num = int(sys.argv[1])
+
+# Check disk count
+if(disk_num < 1):
+    print("Wrong disk number")
 
 # Stacks
 stacka = []
@@ -114,6 +121,40 @@ def printstacks():
 ######################
 ######## Main ########
 ######################
+
+# Banner
+if os.name == "posix":
+    fg = "\033[0;92m"
+    fw = "\033[0;97m"
+else:
+    from colorama import Fore, init
+    init(autoreset=True)
+    fg = Fore.GREEN
+    fw = Fore.WHITE
+
+
+banner = '''{}
+8888888b.           888    888                            d8b 
+888   Y88b          888    888                            Y8P 
+888    888          888    888                                
+888   d88P 888  888 8888888888  8888b.  88888b.   .d88b.  888 
+8888888P"  888  888 888    888     "88b 888 "88b d88""88b 888 
+888        888  888 888    888 .d888888 888  888 888  888 888 
+888        Y88b 888 888    888 888  888 888  888 Y88..88P 888 
+888         "Y88888 888    888 "Y888888 888  888  "Y88P"  888 
+                888                                           
+           Y8b d88P                                           
+            "Y88P"                       
+{}
+Title:    PyHanoi
+Author:   Brandon Vessel
+Github:   https://github.com/brandonvessel/PyHanoi
+Version:  1.0
+Purpose:  Simulate Towers of Hanoi in terminal.
+'''.format(fg, fw)
+
+print(banner)
+sleep(3)
 
 if(__name__ == "__main__"):
     print("-" * 50)
